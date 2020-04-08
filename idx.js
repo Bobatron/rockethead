5f8d422364323aaf58a7a11d04f2fbfacd2b3e35
Cobaloba <me@conor.love>
2020-04-08 22:14:59 +0100

    Updated Index
    16% Extra Hilarity added

--- idx.js
+++ idx.js
@@ -1,11 +1,15 @@
 let stars = [];
 let canvheight = 400
-let img;
-rocketx = -913;
-rockety = 0;
+let imgcon,imgbob,imgscream;
+crocketx = -447;
+crockety = 0;
+brocketx = (-440) + crocketx;
+brockety = 0;
 
 function preload() {
-  img = loadImage('assets/rocket.png');
+  imgcon = loadImage('assets/rocketcon.png');
+  imgbob = loadImage('assets/rocketbob.png');
+  imgscream = loadImage('assets/rocketbobscream.png');
 }
 
 function setup(){
@@ -23,7 +27,7 @@ function draw(){
 	for (i=0;i<1000;i++){
 		stars[i].show();
 	}
-	
+
 	Rocketfly();
 
 }
@@ -33,28 +37,38 @@ class Stars {
     this.x = random(0,width);
     this.y = random(0,canvheight);
 	this.size = random(1,5);
-	
+
   }
-  
-  
+
+
   show(){
 	  stroke("#FFFFFF");
 	  strokeWeight(this.size);
 	  point(this.x, this.y);
   }
-  
+
 
 }
 
 function Rocketfly(){
+crocketx +=2;
+brocketx +=2;
+image(imgcon,crocketx,crockety);
 
-image(img,rocketx,rockety);
-
-rocketx +=2;
-
-if (rocketx >= width){
-	rocketx = -913;
+if ((brocketx + 440) < width/3){
+  image(imgbob,brocketx,brockety);
+} else {
+  image(imgscream,brocketx,brockety);
+  brockety += 2;
 }
-	
-	
+
+if (crocketx >= width){
+	crocketx = -447;
+  brocketx = -887;
+  brockety = 0;
+}
+
+
+
+
 }
