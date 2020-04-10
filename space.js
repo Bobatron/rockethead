5d7ea6bd85e8fce541f8502f2a38d7215cefd113
Cobaloba <me@conor.love>
2020-04-10 23:00:50 +0100

    SPACE
    Space game startin to look pretty rad

--- space.js
+++ space.js
@@ -7,6 +7,8 @@ let shipspeedlimit = 100;
 let ast = [];
 let spacelength = 500;
 let spacewidth = 500;
+let asteroidfieldsize = 10;
+let health = 100;
 
 function preload(){
 	shipimg = loadImage('assets/ship.png');
@@ -28,7 +30,7 @@ function setup() {
   ship = new player();
 
   //asteroid field
-  for(i=0;i<5;i++){
+  for(i=0;i<asteroidfieldsize;i++){
 	  ast[i] = new asteroid();
   }
 
@@ -52,7 +54,6 @@ function draw(){
 	ship.move();
 	ship.shoot();
 
-
 	//bullet movement
 	if (bullets.length > 0){
 		for (i=0;i<bullets.length;i++){
@@ -61,21 +62,26 @@ function draw(){
 		}
 	}
 
-	//asteroid movement
+	//asteroid movement & collisions
 	for (i=0;i<ast.length;i++){
 		ast[i].show();
 		ast[i].move();
+		ast[i].collide(ship.x,ship.y,ship.size);
 		}
 
 	Displayspeed();
+	DisplayHealth();
+
+
+
 
 }
 
 
 class player{
 	constructor(){
-		this.x = 300;
-		this.y = 475;
+		this.x = 235;
+		this.y = 450;
 		this.size = 50;
 
 
@@ -86,7 +92,7 @@ class player{
 	}
 
 	show(){
-		translate(-(this.size/2),-(this.size/2))
+		//translate(-(this.size/2),-(this.size/2))
 		if (keyIsDown(ESCAPE)) {
 			//hyperspeed
 			image(shipimg2,this.x,this.y,this.size, this.size);
@@ -96,6 +102,8 @@ class player{
 		} else {
 			shipspeed = 5;
 			image(shipimg,this.x,this.y,this.size, this.size);
+			// stroke(0);
+			// rect(this.x, this.y, this.size, this.size);
 		}
 
 
@@ -135,9 +143,11 @@ class bullet{
 	}
 
 	show(){
+		push();
 		stroke(this.col);
 		strokeWeight(this.size);
 		point(this.x,this.y);
+		pop();
 
 	}
 
@@ -200,14 +210,29 @@ class asteroid{
 		//translate(-(this.size/2),-(this.size/2))
 		image(astimg,this.x,this.y,this.size, this.size);
 
+
+		// circles to test collision
+		// push();
+		// fill("white");
+		// ellipse(this.x,this.y,this.size);
+		// pop();
+
 	}
 
 	move(){
 		this.y += (shipspeed * 0.2);
 		if (this.y > height){
 			this.x = random(50,width);
-			this.y = random(0,-150);
+			this.y = random(0,-(spacelength));
 			this.size = random(10,100);
+		}
+
+	}
+
+	collide(x,y,s){
+		if (collideRectCircle(x,y,s,s,this.x + (this.size/2),this.y + (this.size/2),this.size)){
+			console.log("HIT!");
+			health -= 1;
 		}
 
 	}
@@ -219,5 +244,26 @@ function Displayspeed(){
 
 }
 
+function DisplayHealth(){
+	if (health > 0){
+		var mappedhealth = map(health,0,100,50,spacewidth - 80);
+		var healthcol;
+		if (health > 70) {healthcol = 'green'} else
+		if (health > 40) {healthcol = 'orange'} else
+		{healthcol = 'red'};
+		fill(healthcol);
+		push();
+		noStroke();
+		text("Health",0,spacelength - 1);
+		pop();
+		rect(50,spacelength - 8,mappedhealth,5)
+		return true;
+	} else {
+		return false;
+	}
+
+
+
+}
 //Increase for each level ->
 //resizeCanvas(windowWidth, windowHeight);
