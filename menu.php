79e226940a5ecb9134a30294083aa57deaa1363f
Cobaloba <me@conor.love>
2020-04-13 23:18:58 +0100

    menu test
    will probably fail

---
+++ menu.php
@@ -0,0 +1,3 @@
+<?php
+echo   '<ul> <li><a class="active" href="index.html">Home</a></li>    <li><a href="conny.html">Cona</a></li>    <li><a href="airCommando/bobby.html">Bobby</a></li>    <li><a href="space.html">Space</a></li>    <li style="float:right"> <a href="#about">Fecks this about?</a></li>  </ul>';
+?>
